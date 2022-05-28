ef824b2b1adbb3d5bcbe40b5e2c046b7eea11699
Enzeng Li <Email>
2022-05-29 00:37:48 +0800

    server——serverModel

---
+++ src/dao/ServerDao.java
@@ -0,0 +1,34 @@
+package dao;
+
+import db.LoginDB;
+import model.ServerModel;
+
+import java.sql.SQLException;
+
+public class ServerDao {
+    ServerModel server;
+    //公告信息
+    LoginDB logindb;
+    //数据库连接
+    public ServerDao(){
+        logindb=new LoginDB();
+    }
+
+
+    /**
+     * 获取公告信息
+     * */
+    public ServerModel getNotice() throws SQLException {
+        server=logindb.getSever();
+
+        return server;
+    }
+
+    /**
+     *
+     * */
+    public int changeNotice(ServerModel ser) throws SQLException {
+        logindb.set_notice(ser);
+        return 0;
+    }
+}
